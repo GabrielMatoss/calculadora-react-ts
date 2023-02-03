ec809d8eed6e5b0e8ee538fb89e4e682336f29dd
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-02-03 18:47:53 -0300

    formatação de código

--- src/components/Calculadora.tsx
+++ src/components/Calculadora.tsx
@@ -1,63 +1,103 @@
-
 import { useState } from "react";
-import { CalculadoraContainer, TecladoDiv, Teclas, VisorCalculo } from "./Calculadora.styles";
-
+import {
+  CalculadoraContainer,
+  TecladoDiv,
+  Teclas,
+  VisorCalculo,
+} from "./Calculadora.styles";
 
 export function Calculadora() {
-    const [current, setCurrent] = useState("");
+  const [current, setCurrent] = useState("");
 
-    function handleClick(event:any) {
-      let keyButton =  event.target.name;
-        //aparentemente eu vou ter que pegar os valores de formas diferentes para cada click
+  function handleClick(event: any) {
+    let keyButton = event.target.name;
+    //aparentemente eu vou ter que pegar os valores de formas diferentes para cada click
+    keyButton;
+    switch (keyButton) {
+      case "+":
+        break;
+      case "-":
+        console.log("O menos funcionando:", keyButton);
+        break;
+      default:
+        console.log(keyButton);
+        break;
+    }
+  }
 
-        switch(keyButton){
-            case "+":
-                let currentNumber = [...keyButton];
-                console.log(currentNumber)
-                break;
-            case "-":
-                console.log("O menos funcionando:", keyButton)
-                break;
-            default:
-                console.log(keyButton);
-                break
-        }
-      
-    }   
-
-    return (
-        <CalculadoraContainer >
-            <VisorCalculo>
-                <div id="visorCalc">
-                    <div id="operations">
-                         <div>1+1</div>
-                         <div id="current_operation">2</div>
-                    </div>
-                    <div>=</div>
-                </div>
-            </VisorCalculo>
-            <TecladoDiv>
-                <Teclas name="CE" onClick={handleClick}>CE</Teclas>
-                <Teclas name="C" onClick={handleClick}>C</Teclas>
-                <Teclas name="%" onClick={handleClick}>%</Teclas>
-                <Teclas name="/" onClick={handleClick}>/</Teclas>
-                <Teclas name="7" onClick={handleClick}>7</Teclas>
-                <Teclas name="8" onClick={handleClick}>8</Teclas>
-                <Teclas name="9" onClick={handleClick}>9</Teclas>
-                <Teclas name="*" onClick={handleClick}>x</Teclas>
-                <Teclas name="4" onClick={handleClick}>4</Teclas>
-                <Teclas name="5" onClick={handleClick}>5</Teclas>
-                <Teclas name="6" onClick={handleClick}>6</Teclas>
-                <Teclas name="-" onClick={handleClick}>-</Teclas>
-                <Teclas name="1" onClick={handleClick}>1</Teclas>
-                <Teclas name="2" onClick={handleClick}>2</Teclas>
-                <Teclas name="3" onClick={handleClick}>3</Teclas>
-                <Teclas name="+" onClick={handleClick}>+</Teclas>
-                <Teclas name="+/-" onClick={handleClick}>+/-</Teclas>
-                <Teclas name="0" onClick={handleClick}>0</Teclas>
-                <Teclas name="," onClick={handleClick}>,</Teclas>
-                <Teclas name="=" onClick={handleClick}>=</Teclas>
-            </TecladoDiv>
-        </CalculadoraContainer>
-    )
+  return (
+    <CalculadoraContainer>
+      <VisorCalculo>
+        <div id="visorCalc">
+          <div id="operations">
+            <div>1+1</div>
+            <div id="current_operation">2</div>
+          </div>
+          <div>=</div>
+        </div>
+      </VisorCalculo>
+      <TecladoDiv>
+        <Teclas name="CE" onClick={handleClick}>
+          CE
+        </Teclas>
+        <Teclas name="C" onClick={handleClick}>
+          C
+        </Teclas>
+        <Teclas name="%" onClick={handleClick}>
+          %
+        </Teclas>
+        <Teclas name="/" onClick={handleClick}>
+          /
+        </Teclas>
+        <Teclas name="7" onClick={handleClick}>
+          7
+        </Teclas>
+        <Teclas name="8" onClick={handleClick}>
+          8
+        </Teclas>
+        <Teclas name="9" onClick={handleClick}>
+          9
+        </Teclas>
+        <Teclas name="*" onClick={handleClick}>
+          x
+        </Teclas>
+        <Teclas name="4" onClick={handleClick}>
+          4
+        </Teclas>
+        <Teclas name="5" onClick={handleClick}>
+          5
+        </Teclas>
+        <Teclas name="6" onClick={handleClick}>
+          6
+        </Teclas>
+        <Teclas name="-" onClick={handleClick}>
+          -
+        </Teclas>
+        <Teclas name="1" onClick={handleClick}>
+          1
+        </Teclas>
+        <Teclas name="2" onClick={handleClick}>
+          2
+        </Teclas>
+        <Teclas name="3" onClick={handleClick}>
+          3
+        </Teclas>
+        <Teclas name="+" onClick={handleClick}>
+          +
+        </Teclas>
+        <Teclas name="+/-" onClick={handleClick}>
+          +/-
+        </Teclas>
+        <Teclas name="0" onClick={handleClick}>
+          0
+        </Teclas>
+        <Teclas name="," onClick={handleClick}>
+          ,
+        </Teclas>
+        <Teclas name="=" onClick={handleClick}>
+          =
+        </Teclas>
+      </TecladoDiv>
+    </CalculadoraContainer>
+  );
 }
