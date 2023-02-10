b0c7f2a35b34270b9a16ddbd26eb56955a99bbfe
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-02-10 09:31:17 -0300

    desafio concluído

--- src/components/Calculadora.tsx
+++ src/components/Calculadora.tsx
@@ -6,118 +6,81 @@ import {
   VisorCalculo,
 } from "./Calculadora.styles";
 
-
 export function Calculadora() {
-  const [current, setCurrent] = useState("");
-  const [current2, setCurrent2] = useState("");
-  function handleClick(event: any) {
-    let keyButton = event.target.name;
-    //aparentemente eu vou ter que pegar os valores de formas diferentes para cada click
-    
-    /*
-      Podemos tentar capturar o clique e jogar no primeiro numero do calculo, 
-      depois capturamos os sinal, e depois o ultimo numero, 
-      para aí sim mostrar o resultado se clicarmos no botao de igual.
-     */
-
-      /*
-        podemos fazer uma verificação direto no keyButton se ele incluir esses operadores a gente quita ele
-      */
-    const notNumber: any = {
-      C: "C",
-      CE: "CE",  
-    }
-
-    let verifySimple = () => {
-      for(let simbols in notNumber){
-       return(notNumber[simbols]) 
-      }
-      }
-
-   return setCurrent(():any => {
-    if(keyButton.includes(verifySimple())){
-      return "Deu Ruim";
-    }else {
-      return keyButton;
-    }
-    
-   });
-  }
-
   return (
     <CalculadoraContainer>
       <VisorCalculo>
         <div id="visorCalc">
           <div id="operations">
             <div>
-              <span>{current}</span>
+              <span>2</span>
               <span>+</span>
-              <span>{current2}</span>
+              <span>2</span>
             </div>
-            <div id="current_operation">2</div>
+            <div id="current_operation">4</div>
           </div>
           <div>=</div>
         </div>
       </VisorCalculo>
       <TecladoDiv>
-        <Teclas name="CE" onClick={handleClick}>
+        <Teclas name="CE" >
           CE
         </Teclas>
-        <Teclas name="C" onClick={handleClick}>
+        <Teclas name="C" >
           C
         </Teclas>
-        <Teclas name="%" onClick={handleClick}>
+        <Teclas name="%" >
           %
         </Teclas>
-        <Teclas name="/" onClick={handleClick}>
+        <Teclas name="/" >
           /
         </Teclas>
-        <Teclas name="7" onClick={handleClick}>
+        <Teclas name="7" >
           7
         </Teclas>
-        <Teclas name="8" onClick={handleClick}>
+        <Teclas name="8" >
           8
         </Teclas>
-        <Teclas name="9" onClick={handleClick}>
+        <Teclas name="9" >
           9
         </Teclas>
-        <Teclas name="*" onClick={handleClick}>
+        <Teclas name="*" >
           x
         </Teclas>
-        <Teclas name="4" onClick={handleClick}>
+        <Teclas name="4" >
           4
         </Teclas>
-        <Teclas name="5" onClick={handleClick}>
+        <Teclas name="5" >
           5
         </Teclas>
-        <Teclas name="6" onClick={handleClick}>
+        <Teclas name="6" >
           6
         </Teclas>
-        <Teclas name="-" onClick={handleClick}>
+        <Teclas name="-" >
           -
         </Teclas>
-        <Teclas name="1" onClick={handleClick}>
+        <Teclas name="1" >
           1
         </Teclas>
-        <Teclas name="2" onClick={handleClick}>
+        <Teclas name="2" >
           2
         </Teclas>
-        <Teclas name="3" onClick={handleClick}>
+        <Teclas name="3" >
           3
         </Teclas>
-        <Teclas name="+" onClick={handleClick}>
+        <Teclas name="+" >
           +
         </Teclas>
-        <Teclas name="+/-" onClick={handleClick}>
+        <Teclas name="+/-" >
           +/-
         </Teclas>
-        <Teclas name="0" onClick={handleClick}>
+        <Teclas name="0" >
           0
         </Teclas>
-        <Teclas name="," onClick={handleClick}>
+        <Teclas name="," >
           ,
         </Teclas>
-        <Teclas name="=" onClick={handleClick}>
+        <Teclas name="=" >
           =
         </Teclas>
       </TecladoDiv>
